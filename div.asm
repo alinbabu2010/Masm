04509de574643b2e4d18adb8f897056c63d4244d
ALIN BABU <alinbabu2010@gmail.com>
2019-04-08 20:42:03 +0530

    Add files via upload

--- div.asm
+++ div.asm
@@ -1,106 +1,106 @@
-DATA SEGMENT
-    MESS1 DB 0AH,0DH,'ENTER THE DIVIDEND:','$'
-    MESS2 DB 0AH,0DH,'ENTER THE DIVISOR:','$'
-    MESS3 DB 0AH,0DH,'DIVISION IS :','$'
-DATA ENDS
-CODE SEGMENT
-    ASSUME CS:CODE,DS:DATA
-    START:  MOV AX,DATA
-            MOV DS,AX
-            LEA DX,MESS1
-            MOV AH,09H
-            INT 21H
-            CALL READ
-            MOV BX,DX
-            LEA DX,MESS2
-            MOV AH,09H
-            INT 21H
-            CALL READ
-            MOV SI,DX
-            MOV DX,0000H
-            MOV AX,BX
-            DIV SI
-            MOV BX,AX
-            MOV AX,0000H
-            DIV SI
-            MOV SI,AX
-            CALL DISP
-            MOV AH,4CH
-            INT 21H
-            READ PROC NEAR 
-            PUBLIC READ
-            MOV CH,02H
-    R3:     MOV AH,01H
-            INT 21H
-            MOV CL,04H
-            MOV DL,AL
-            SUB DL,30H
-            CMP DL,0AH
-            JC R1 
-            SUB DL,07H
-    R1:     SHL DL,CL
-            MOV AH,01H
-            INT 21H
-            SUB AL,30H
-            CMP AL,0AH
-            JC R2
-            SUB AL,07H
-            AND AL,0FH
-    R2:     OR DL,AL
-            DEC CH
-            JZ R4
-            MOV DH,DL
-            JMP R3
-    R4:     RET
-            READ ENDP
-            DISP PROC NEAR 
-            PUBLIC DISP
-            LEA DX,MESS3
-            MOV AH,09H
-            INT 21H
-            MOV CH,02H
-            MOV CL,04H
-            MOV DL,BH
-            SHR DL,CL
-            CMP DL,0AH
-            JC L1
-            ADD DL,07H
-    L1:     ADD DL,30H
-            MOV AH,06H
-            INT 21H
-            AND BH,0FH
-            CMP BH,0AH
-            JC L2
-            ADD BH,07H
-    L2:     ADD BH,30H
-            MOV DL,BH
-            MOV AH,06H
-            INT 21H
-    BACK:   MOV DL,BL
-            SHR DL,CL
-            CMP DL,0AH
-            JC L3 
-            ADD DL,07H
-    L3:     ADD DL,30H
-            MOV AH,06H
-            INT 21H
-            AND BL,0FH
-            CMP BL,0AH
-            JC L4
-            ADD BL,07H
-    L4:     ADD BL,30H
-            MOV DL,BL
-            MOV AH,06H
-            INT 21H
-            DEC CH
-            JZ NXT
-            MOV DL,'.'
-            MOV AH,06H
-            INT 21H
-            MOV BX,SI
-            MOV BL,BH
-            JMP BACK  
-    NXT:    RET
-    DISP ENDP
-CODE ENDS
-END START
+data segment
+    mess1 db 0ah,0dh,'Enter the dividend:','$'
+    mess2 db 0ah,0dh,'Enter the divisor:','$'
+    mess3 db 0ah,0dh,'Division is :','$'
+data ends
+code segment
+assume cs:code,ds:data
+start:  mov ax,data
+        mov ds,ax
+        lea dx,mess1
+        mov ah,09h
+        int 21h
+        call read
+        mov bx,dx
+        lea dx,mess2
+        mov ah,09h
+        int 21h
+        call read
+        mov si,dx
+        mov dx,0000h
+        mov ax,bx
+        div si
+        mov bx,ax
+        mov ax,0000h
+        div si
+        mov si,ax
+        call disp
+        mov ah,4ch
+        int 21h
+        read proc near 
+        public read
+        mov ch,02h
+r3:     mov ah,01h
+        int 21h
+        mov cl,04h
+        mov dl,al
+        sub dl,30h
+        cmp dl,0ah
+        jc r1 
+        sub dl,07h
+r1:     shl dl,cl
+        mov ah,01h
+        int 21h
+        sub al,30h
+        cmp al,0ah
+        jc r2
+        sub al,07h
+        and al,0fh
+r2:     or dl,al
+        dec ch
+        jz r4
+        mov dh,dl
+        jmp r3
+r4:     ret
+        read endp
+        disp proc near 
+        public disp
+        lea dx,mess3
+        mov ah,09h
+        int 21h
+        mov ch,02h
+        mov cl,04h
+        mov dl,bh
+        shr dl,cl
+        cmp dl,0ah
+        jc l1
+        add dl,07h
+l1:     add dl,30h
+        mov ah,06h
+        int 21h
+        and bh,0fh
+        cmp bh,0ah
+        jc l2
+        add bh,07h
+l2:     add bh,30h
+        mov dl,bh
+        mov ah,06h
+        int 21h
+back:   mov dl,bl
+        shr dl,cl
+        cmp dl,0ah
+        jc l3 
+        add dl,07h
+l3:     add dl,30h
+        mov ah,06h
+        int 21h
+        and bl,0fh
+        cmp bl,0ah
+        jc l4
+        add bl,07h
+l4:     add bl,30h
+        mov dl,bl
+        mov ah,06h
+        int 21h
+        dec ch
+        jz nxt
+        mov dl,'.'
+        mov ah,06h
+        int 21h
+        mov bx,si
+        mov bl,bh
+        jmp back  
+nxt:    ret
+        disp endp
+code ends
+end start
